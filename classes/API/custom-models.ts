173400307ebc77057fc1e160fcbdd6cad29c5e04
Halil Savaşcı <halilsavasci32@gmail.com>
2022-11-17 16:44:05 +0300

    model improvement

--- classes/API/custom-models.ts
+++ classes/API/custom-models.ts
@@ -5,7 +5,7 @@ export const GetImageInputForAPI = Z.object({
     background: Z.string().optional(),
     compressionLevel: Z.preprocess((val: any) => {
         return parseInt(val as any)
-    }, Z.number()).optional(),
+    }, Z.number().min(0).max(9)).optional(),
     filename: Z.string(),
     fit: Z.nativeEnum(Fit).optional(),
     format: Z.nativeEnum(Format).optional(),
@@ -14,7 +14,7 @@ export const GetImageInputForAPI = Z.object({
     }, Z.number()).optional(),
     quality: Z.preprocess((val: any) => {
         return parseInt(val as any)
-    }, Z.number()).optional(),
+    }, Z.number().min(1).max(100)).optional(),
     width: Z.preprocess((val: any) => {
         return parseInt(val as any)
     }, Z.number()).optional(),
